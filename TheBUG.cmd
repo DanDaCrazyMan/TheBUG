7d38ab254d4da94da14057c35543a6b39c334f18
DanDaCrazyMan <35356645+DanDaCrazyMan@users.noreply.github.com>
2018-01-12 22:33:38 +1000

    Create TheBUG.cmd

---
+++ TheBUG.cmd
@@ -0,0 +1,6 @@
+@echo off
+title Dev
+color 1b
+echo Sorry! Check back later. This program is nothing but a waste of drive space. If you were to start the program up, it
+echo would probably destroy your computer. For now, you can look at this message! Very exciting.
+pause
